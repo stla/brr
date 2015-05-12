a95840b39d3e6b89653973b5fb4edbfb02dcae2f
stla <laurent_step@yahoo.fr>
2015-05-12 09:23:13 +0200

    Prior_phi

--- R/distributions.R
+++ R/distributions.R
@@ -97,3 +97,69 @@ rprior_mu<-function(n, a, b, ...){
 }
 
 
+#' @name Prior_phi 
+#' @rdname Prior_phi
+#' @title Prior distribution on the relative risk and the vaccine efficacy
+#' 
+#' @details The prior distribution on the relative risk \eqn{\phi} is the Beta2 distribution 
+#' with shape parameters \eqn{c} and \eqn{d} and scale parameter \eqn{(T+b)/S}.
+#' 
+#' @param x,q vector of quantiles 
+#' @param p vector of probabilities
+#' @param b non-negative rate parameter
+#' @param c,d non-negative shape parameters 
+#' @param S,T sample sizes in control group and treated group
+#' @param n number of observations to be simulated
+#' @param ... other arguments passed to \code{\link{Beta2Dist}}
+#' 
+#' @return \code{dprior_phi} gives the density, \code{pprior_phi} the distribution function, \code{qprior_phi} the quantile function, and \code{rprior_phi} samples from the distribution.
+#' 
+#' @note \code{Prior_phi} is a generic name for the functions documented. 
+#' 
+#' @examples 
+#' curve(dprior_phi(x, 2, 2, 2, 10, 10), from=0, to=7)
+#' 
+NULL
+
+#' @rdname Prior_phi
+#' @export 
+dprior_phi<-function(phi, b, c, d, S, T, ...){
+  scale <- (T+b)/S
+  dbeta2(phi,c,d,scale, ...)
+}
+#' @rdname Prior_phi
+#' @export 
+dprior_VE<-function(VE, b, c, d, S, T, ...){
+  dprior_phi(1-VE, b, c, d, S, T, ...)
+}
+#'
+#' @rdname Prior_phi
+#' @export 
+pprior_phi<-function(q, b, c, d, S, T, ...){
+  scale <- (T+b)/S
+  pbeta2(q,c,d,scale, ...)
+}
+#' @rdname Prior_phi
+#' @export 
+pprior_VE<-function(q, b, c, d, S, T, ...){
+  1-pprior_phi(1-q, b, c, d, S, T, ...)
+}
+
+#'
+#' @rdname Prior_phi
+#' @export 
+qprior_phi <- function(p, b, c, d, S, T, ...){
+  scale <- (T+b)/S
+  qbeta2(p, c, d, scale, ...)
+}
+#' @rdname Prior_phi
+#' @export 
+qprior_VE <- function(p, b, c, d, S, T, ...){
+  1-qprior_phi(1-p, b, c, d, S, T, ...)
+}
+#'
+#' @rdname Prior_phi
+#' @export 
+rprior_phi<-function(n, b, c, d, S, T){
+  rbeta2(n, c, d, scale=(T+b)/S)
+}
