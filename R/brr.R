3255e54a7cbdb1956fb82290647e67497aeb5847
stla <laurent_step@yahoo.fr>
2015-06-09 11:29:22 +0200

    prior type

--- R/brr.R
+++ R/brr.R
@@ -46,6 +46,24 @@ plot.brr <- function(brr){ # marche car plot a déjà méthode S3 ; test.brr mar
   # faire output ggplots qui s'affichent ou liste de ggplot
 }
 
+#' Type of the prior
+#' 
+#' 
+prior <- function(params){
+  #params <- brr()
+  if(all(c("a","b","c","d","S","T") %in% names(params))){
+    return("informative prior")
+  }else{
+    if(!all(c("a","b") %in% names(params))){
+      return("non-informative prior")
+    }else{
+      if(all(c("a","b") %in% names(params))){
+        return("semi-informative prior")
+      }
+    }
+  }
+}
+
 #' Summary brr
 #' 
 #' @examples
@@ -55,15 +73,34 @@ plot.brr <- function(brr){ # marche car plot a déjà méthode S3 ; test.brr mar
 #' summary(model)
 #' model <- brr(a=2, b=4, T=10)
 #' summary(model)
+#' model <- brr(a=2, b=4, c=3, d=5, S=10, T=10)
+#' summary(model)
 #' @export
 summary.brr <- function(brr){
   params <- brr()
+  type <- prior(params)
   cat("----------\n")
+  cat(type)
+  cat("\n\n")
   cat("*Prior distribution on µ*\n")
   if(all(c("a","b") %in% names(params))){
-    cat(with(params, sprintf("Gamma(a=%s,b=%s)\n", a, b)))
+    cat(with(params, sprintf("  Gamma(a=%s,b=%s)", a, b)))
   }else{
-    cat("a and b not supplied")
+    cat("  Non-informative prior")
   }
+  cat("\n\n")
+  cat("*Prior distribution on phi*\n")
+  if(all(c("c","d","b","S","T") %in% names(params))){
+    cat(with(params, sprintf("  Beta2(c=%s,d=%s,scale=%s)", c, d, (T+b)/S)))
+  }else{
+    if(type=="non-informative prior" || type=="semi-informative prior"){
+      cat("  Non-informative prior")
+    }else{
+      cat("  c, d, b, S and T must be supplied")
+    }
+  }
+  cat("\n\n")
+  cat("*Sample sizes*\n")
+  cat(sprintf("  T (control group): %s", ifelse("T" %in% names(params), params$T, "not supplied")))
   return(invisible())
 }
